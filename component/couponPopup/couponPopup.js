4567c8e129ee253f521c5a277dfd850d81bba6a4
HumYellow <ethdgdg@126.com>
2019-06-12 17:32:59 +0800

    2019-6-12

---
+++ component/couponPopup/couponPopup.js
@@ -0,0 +1,32 @@
+// component/couponPopup/couponPopup.js
+const app = getApp()
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    couponPopupShow: {
+      type: Boolean,
+      value: false,
+      observer: function (newVal, oldVal, changedPath) {
+        
+
+      }
+    },
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
